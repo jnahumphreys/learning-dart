699c0ee8e0b9eb8af20a1e65ca1ab07d52075d98
James Humphreys <9253831+jnahumphreys@users.noreply.github.com>
2026-05-07 13:49:39 +0100

    dart-learn/chapter-eight

--- dartpedia/command_runner/lib/src/command_runner_base.dart
+++ dartpedia/command_runner/lib/src/command_runner_base.dart
@@ -6,22 +6,27 @@ import 'arguments.dart';
 import 'exceptions.dart';
 
 class CommandRunner {
-  CommandRunner({this.onError});
+  CommandRunner({this.onOutput, this.onError});
 
   final Map<String, Command> _commands = <String, Command>{};
 
   UnmodifiableSetView<Command> get commands =>
       UnmodifiableSetView<Command>(<Command>{..._commands.values});
 
+  FutureOr<void> Function(String)? onOutput;
+
   FutureOr<void> Function(Object)? onError;
 
   Future<void> run(List<String> input) async {
-    // [Step 6 update] try/catch added
     try {
       final ArgResults results = parse(input);
       if (results.command != null) {
         Object? output = await results.command!.run(results);
-        print(output.toString());
+        if (onOutput != null) {
+          await onOutput!(output.toString());
+        } else {
+          print(output.toString());
+        }
       }
     } on Exception catch (exception) {
       if (onError != null) {
